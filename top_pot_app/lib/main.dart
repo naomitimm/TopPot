2b365784ee6c7ada3fddb97ba42d32be73cd22e1
Naomi Timothy <naomitim17.com>
2022-10-31 01:07:31 +0300

    initial-commit

---
+++ top_pot_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TopPotApp());
+}
+
+class TopPotApp extends StatelessWidget {
+  const TopPotApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
