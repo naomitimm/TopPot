4ba62860b9fed773e5eb4d251bb44f2bf1851b76
Naomi Timothy <naomitim17.com>
2022-10-31 21:57:38 +0300

    land edit

--- top_pot_app/lib/presentation/exports.dart
+++ top_pot_app/lib/presentation/exports.dart
@@ -1,5 +1,8 @@
 // packages
 export 'package:flutter/material.dart';
 export 'package:top_pot_app/presentation/auth/widgets/waves.dart';
+export 'package:google_fonts/google_fonts.dart';
 
 // pages
+export 'package:top_pot_app/presentation/auth/widgets/buttons.dart';
+export 'package:top_pot_app/presentation/auth/widgets/texts.dart';
