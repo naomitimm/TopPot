f6e90729bc4cbd4c97fad91a10c2c2bd9198ed7b
Naomi Timothy <naomitim17.com>
2022-11-01 18:54:58 +0300

    navpages setup

---
+++ top_pot_app/lib/presentation/nav/profile_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:top_pot_app/presentation/exports.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown,
+    );
+  }
+}
