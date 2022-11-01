0b4e3a2dcd52e30a15cf39c980dce0bb37996738
Naomi Timothy <naomitim17.com>
2022-11-01 18:54:36 +0300

    nav-pages-setup

---
+++ top_pot_app/lib/presentation/nav/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:top_pot_app/presentation/exports.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink,
+    );
+  }
+}
--- top_pot_app/lib/presentation/nav/host_page.dart
+++ top_pot_app/lib/presentation/nav/host_page.dart
@@ -1,13 +1,8 @@
 import 'package:top_pot_app/presentation/exports.dart';
 
-class HostPage extends StatefulWidget {
+class HostPage extends StatelessWidget {
   const HostPage({Key? key}) : super(key: key);
 
-  @override
-  State<HostPage> createState() => _HostPageState();
-}
-
-class _HostPageState extends State<HostPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
